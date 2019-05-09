82b795f953942f9b749456430a447cfa3421af74
sasanuma hayato <h.sasanuma@cci.co.jp>
2019-05-09 17:13:34 +0900

    initialize

---
+++ emacs.d/elpa/git-commit-20190413.1201/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20190413.1201" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (with-editor "20181103")) :commit "b5e43ac63032c8cd3fe085c408cfd3df1d82e5c3" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
