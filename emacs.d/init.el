ec2d966e0aa2c8abef08ce537d57a08cebff82de
flik58 <craft.sh58@gmail.com>
2019-06-27 22:19:49 +0900

    update

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,4 +1,11 @@
-;; package
+;;(mac-add-ignore-shortcut '(control))
+;;(setq x-select-enable-clipboard t)
+
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
 (package-initialize)
 (setq package-archives
       '(("gnu" . "http://elpa.gnu.org/packages/")
@@ -14,77 +21,11 @@
         (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
             (normal-top-level-add-subdirs-to-load-path))))))
 
-;; 環境を日本語、UTF-8にする
-(set-locale-environment nil)
+(setq default-tab-width 4)
+(define-key global-map [?¥] [?\\])  ;; ¥の代わりにバックスラッシュを入力する
 
-;; 各種設定
-; バックアップなし
-; (setq make-backup-files nil)
-; (setq auto-save-default nil)
-
-(setq inhibit-startup-screen t)
-(put 'narrow-to-region 'disabled nil)
-
-; indent
-(setq-default indent-tabs-mode nil)
-(setq-default c-basic-offset 4
-              tab-width 4
-              indent-tabs-mode nil)
-
-; web-mode
-(require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.[gj]sp\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(setq web-mode-engines-alist
-      '(("php"    . "\\.phtml\\'")
-        ("blade"  . "\\.blade\\.")))
-
-(defun web-mode-indent (num)
-  (interactive "nIndent: ")
-  (setq web-mode-markup-indent-offset num)
-  (setq web-mode-css-indent-offset num)
-  (setq web-mode-style-padding num)
-  (setq web-mode-code-indent-offset num)
-  (setq web-mode-script-padding num)
-  (setq web-mode-block-padding num)
-  )
-(web-mode-indent 2)
-
-; yaml-mode
-(require 'yaml-mode)
-(add-to-list 'auto-mode-alist '("\\.dig\\'" . yaml-mode))
-
-
-(require 'flymake-python-pyflakes)
-(require 'flymake-cursor)
-(add-hook 'python-mode-hook 'flymake-python-pyflakes-load)
-
-(defun tnoda/turn-on-flycheck-mode ()
-  (flycheck-mode 1))
-;(add-hook 'python-mode-hook 'tnoda/turn-on-flycheck-mode)
-(add-hook 'after-init-hook #'global-flycheck-mode)
-
-;; (when (require 'flymake-python nil t)
-;;   (setq flymake-python-syntax-checker "flake8")
-;;   )
-
-;; (require 'python-mode)
-;; ;; (add-to-list 'auto-mode-alist '("\\\.py\\\'" . python-mode))
-;; (add-to-list 'interpreter-mode-alist '("python" . python-mode))
-
-;; (flymake-mode t)
-;; ; python-mode
-;; (require 'flymake-python-pyflakes)
-;; ;; (global-flymake-python-pyflakes-mode 1)
-;; (add-hook 'python-mode-hook 'flymake-python-pyflakes-load)
-;; (setq flymake-python-pyflakes-executable "flake8")
-
+(setq ring-bell-function 'ignore)
+(define-key global-map (kbd "C-t") 'other-window)
 
 ;\左に行番号表示
 (require 'linum)
@@ -92,76 +33,50 @@
 (column-number-mode t)
 (setq linum-format "%4d ")
 
-; highlight
-;; (defface my-hl-line-face
-;;   '((((class color) (background dark))  ; カラーかつ, 背景が dark ならば,
-;;      (:background "DarkSlateBlue" t))   ; 背景を黒に.
-;;     (((class color) (background light)) ; カラーかつ, 背景が light ならば,
-;;      (:background "gray90" t))     ; 背景を ForestGreen に.
-;;      ;; (:background "light cyan" t))     ; 背景を ForestGreen に.
-;;     (t (:bold t)))
-;;   "hl-line's my face")
-;; (setq hl-line-face 'my-hl-line-face)
-;; (global-hl-line-mode t)
-
-(setq show-paren-delay 0)
-(show-paren-mode t)
-(setq show-paren-style 'expression)
-(set-face-background 'show-paren-match-face nil)
-(set-face-underline-p 'show-paren-match-face "yellow")
-
+;; ハイライト
 ; 行末の空白をハイライト
 (setq-default show-trailing-whitespace t)
 
-;; User specific define-key
+; 現在行のハイライト
+(defface hlline-face
+  '((((class color)
+      (background dark))
+     ;; (:background "dark slate gray"))
+     (:background "gray20"))
+    (((class color)
+      (background light))
+     ;; (:background "ForestGreen"))
+     (:background "gray20"))
+    (t
+     ()))
+  "*Face used by hl-line.")
+(setq hl-line-face 'hlline-face)
+;; (setq hl-line-face 'underline) ; 下線
+(global-hl-line-mode)
+
+; 対応する括弧のハイライト
+(show-paren-mode t)
+(setq show-paren-delay 0)
+;; (setq show-paren-style 'expression)
+(setq show-paren-style 'mixed)
+
 ; 矩形選択
 (cua-mode t)
 (setq cua-enable-cua-keys nil)
 (define-key global-map (kbd "C-x SPC") 'cua-set-rectangle-mark)
 
-(require 'tabbar)
-(tabbar-mode 1)
-;; (global-set-key "" 'tabbar-forward)  ; 次のタブ
-;; (global-set-key "\M-[" 'tabbar-backward) ; 前のタブ
-; タブ上でマウスホイールを使わない
-(tabbar-mwheel-mode nil)
-; グループを使わない
-(setq tabbar-buffer-groups-function nil)
-; 左側のボタンを消す
-(dolist (btn '(tabbar-buffer-home-button
-               tabbar-scroll-left-button
-               tabbar-scroll-right-button))
-  (set btn (cons (cons "" nil)
-                (cons "" nil))))
-; 色設定
-(set-face-attribute ; バー自体の色
- 'tabbar-default nil
- :background "white"
- :family "Inconsolata"
- :height 1.0)
-(set-face-attribute ; アクティブなタブ
- 'tabbar-selected nil
- :background "black"
- :foreground "white"
- :weight 'bold
- :box nil)
-(set-face-attribute ; 非アクティブなタブ
- 'tabbar-unselected nil
- :background "white"
- :foreground "black"
- :box nil)
-
-(setq ring-bell-function 'ignore)
-(define-key global-map (kbd "C-t") 'other-window)
-
 (global-set-key [f6] 'linum-mode)
-(global-set-key [f7] 'tabbar-mode)
-;; (global-set-key [f8] 'flymake-python-pyflakes)
 (global-set-key [f12] 'shell)
-;; (global-set-key [f10] 'gdb)
-;; (global-set-key [f11] 'pdb)
 
+; yaml-mode
+(require 'yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.dig\\'" . yaml-mode))
 
+;; golang
+(add-hook 'go-mode-hook (lambda()
+			  (setq indent-tabs-mode nil)
+			  (setq c-basic-offset 4)
+			  (setq tab-width 4)))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -169,7 +84,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (magit go-errcheck golint go-mode flymake-yaml yaml-mode flycheck flymake-cursor python-mode flymake-python-pyflakes package-utils web-mode tabbar))))
+    (go-errcheck flymake-go yaml-mode python-mode golint go-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
