c818a25f4964240b8694e9686939dd96bfce9371
flik58 <craft.sh58@gmail.com>
2019-06-27 22:29:28 +0900

    modified

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -77,6 +77,9 @@
 			  (setq indent-tabs-mode nil)
 			  (setq c-basic-offset 4)
 			  (setq tab-width 4)))
+
+(add-hook 'go-mode-hook 'flycheck-mode)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -84,7 +87,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (go-errcheck flymake-go yaml-mode python-mode golint go-mode))))
+    (flycheck go-errcheck flymake-go yaml-mode python-mode golint go-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
